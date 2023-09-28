32bf6d8cc27ffc27ba62e69b3975ffc9b2623243
Cannon Lock <clock@wisc.edu>
2023-09-28 08:51:29 -0500

    Update file and data

--- ror.py
+++ ror.py
@@ -9,9 +9,6 @@ def search_ror(row):
         d = response.json()
         for i, item in enumerate(d['items']):
 
-            if i == 3:
-                continue
-
             row[f"Name_{str(i).zfill(2)}"] = item['name']
             row[f"ID_{str(i).zfill(2)}"] = item['id']
     else:
@@ -20,7 +17,30 @@ def search_ror(row):
 
     return row
 
+
+def get_columns(first, number_of_columns):
+
+    columns = [*first]
+
+    for i in range(number_of_columns):
+        columns.append(f"Name_{str(i).zfill(2)}")
+        columns.append(f"ID_{str(i).zfill(2)}")
+
+    return columns
+
+
+
+
 if __name__ == "__main__":
     df = pd.read_json("data/2023-09-12-final_w_supplemental.json")
+
+    # Search for the ROR ID
+
     df = df.apply(lambda x: search_ror(x), axis=1)
+
+    # Reorder nicely
+    columns = get_columns(["Institution Name"], 5)
+    df = df[columns]
+
+    # Write to excel
     df.to_excel("ror_search.xlsx", index=False)
